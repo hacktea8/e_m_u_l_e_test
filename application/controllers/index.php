f689065b3a0e6433e2983535ff08b94b1708ad8a
hacktea8 <admin@hacktea8.com>
2014-02-09 22:17:10 +0800

    Init

---
+++ application/controllers/index.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once 'usrbase.php';
+class Index extends Usrbase {
+   
+	/**
+	 * Index Page for this controller.
+	 *
+	 */
+  public function __construct(){
+    parent::__construct();
+    $this->load->model('indexmodel');
+  }
+  public function index()
+  {
+    $this->load->view('index_index',$this->viewData);
+  }
+  public function tpl(){
+    $this->load->view('index_tpl',$this->viewData);
+  }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
