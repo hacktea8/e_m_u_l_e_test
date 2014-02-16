51e652b6b7dd118d694a4f69b24d750b79817cda
hacktea8 <admin@hacktea8.com>
2014-02-16 20:05:01 +0800

    add google sitemap

--- cront/task/google_sitemap.php
+++ cront/task/google_sitemap.php
@@ -26,53 +26,51 @@ class model{
     $sql = sprintf('SELECT   `index` FROM `pw_emule_sitemap` WHERE `type`=%d',$type);
     return $this->db->result_array($sql);
   }
-  public function getMaxAid($type){
+  public function getMaxIndex($type){
     $sql = sprintf('SELECT   `index` FROM `pw_emule_sitemap` WHERE `type`=%d ORDER BY `id` DESC  LIMIT 1',$type);
     $row = $this->db->row_array($sql);
     return isset($row['index'])?$row['index']:0;
+  }
+  public function getMaxAid($type){
+    $sql = sprintf('SELECT   `aid` FROM `pw_emule_sitemap` WHERE `type`=%d ORDER BY `id` DESC  LIMIT 1',$type);
+    $row = $this->db->row_array($sql);
+    return isset($row['aid'])?$row['aid']:0;
   }
 }
 
 $type = 1;
 $base_url = 'http://emu.hacktea8.com/';
 $model = new model();
-$sitemap_index = '<?xml version="1.0" encoding="UTF-8"?>
-   <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-$index = 1;
-
+$index = $model->getMaxIndex($type) + 1;
 $aid = $model->getMaxAid($type);
+//var_dump($index.'|'.$aid);exit;
 $count = 1;
-$countLimit = 50000;
-  $sitemap = '<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+$countLimit = 30000;
+  $sitemap = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 $tmp = '';
 for($p = 1;;$p++){
  $list = $model->getList($p,150,$aid);
  foreach($list as $val){
-   $tmp .= ' <url>
-    <loc>'.$base_url.article_url($val['id']).'</loc>
-    <lastmod>'.date('Y-m-d H:i:s',$val['utime']).'</lastmod>
-    <changefreq>weekly</changefreq>
-    </url>';
-   if($count > $countLimit){
+   $tmp .= '<url><loc>'.$base_url.article_url($val['id']).'</loc><lastmod>'.date('Y-m-d H:i:s',$val['utime']).'</lastmod><changefreq>never</changefreq></url>';
+   $count++;
+  }
+   if($count > $countLimit || (empty($list) && $tmp)){
       $tmp = $sitemap.$tmp.'</urlset>';
       $index_file = BASEPATH.'google_sitemap'.$index.'.xml';
-      file_put_contents($index_file,$sitemap);
+      file_put_contents($index_file,$tmp);
       $model->addIndex(array('type'=>$type,'index'=>$index,'aid'=>$val['id'],'update'=>$val['utime']));
       $index++;
       $count = 0;
       $tmp = '';
 sleep(5);
    }
-   $count++;
-  }
 }
+
+$sitemap_index = '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
 $indexList = $model->getIndexList($type);
-for($i = 1; $i<=$index; $i++){
-  $sitemap_index .= '<sitemap>
-      <loc>'.$base_url.'google_sitemap'.$i.'.xml</loc>
-      <lastmod>'.date('Y-m-d H:i:s').'</lastmod>
-   </sitemap>';
+foreach($indexList as $val){
+  $sitemap_index .= '<sitemap><loc>'.$base_url.'google_sitemap'.$val['index'].'.xml</loc><lastmod>'.date('Y-m-d H:i:s').'</lastmod></sitemap>';
 }
 
 $sitemap_index .= '</sitemapindex>';
